03cf03625b97ea7e28e0629876e7ddf3ca421009
JulianaChM <julianchamorromarin@gmail.com>
2025-02-23 16:19:00 -0500

    feat: frontend del navbar

--- Frontend/src/app/app.component.ts
+++ Frontend/src/app/app.component.ts
@@ -1,25 +1,44 @@
 import { Component, OnInit } from '@angular/core';
 import { ApiService } from './services/api.service';
-import { RouterOutlet } from '@angular/router';
+import { NavigationEnd, RouterOutlet } from '@angular/router';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import {MatTabsModule} from '@angular/material/tabs';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet, 
+    MatToolbarModule, 
+    MatTabsModule
+  ],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
   message = '';
   title = '💓';
+  rutas = ['/', '/chatbot', '/sobre-nosotros', '*']
+  selectedIndex = 0;
 
-  constructor(private apiService: ApiService) { 
+  constructor(private apiService: ApiService, private router: Router) { 
     console.log("Angular se está ejecutando correctamente");
+
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        this.selectedIndex = this.rutas.indexOf(event.url); // Ajustar índice según la ruta actual
+      }
+    });
   }
 
   ngOnInit() {
     this.apiService.getMessage().subscribe(response => {
       this.message = response.message;
     });
+  }
+
+  cambiarRuta(index: number) {
+    this.router.navigate([this.rutas[index]]);
   }
 }
--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -1,17 +1,34 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { ChatbotComponent } from './chatbot/chatbot.component';
 
 
 export const routes: Routes = [
     {
       path: 'home',
       component: HomeComponent,
-      /* data: { public: true } */
+      data: { public: true }
     },
-/*     {
+    {
       path: '',
       redirectTo: 'home',
       pathMatch: 'full',
       data: { public: true }
-    } */
+    },
+    {
+      path: '*',
+      redirectTo: 'home',
+      pathMatch: 'full',
+      data: { public: true }
+    },
+    {
+      path: 'chatbot',
+      component: ChatbotComponent,
+      data: { public: true }
+    },
+    {
+      path: 'sobre-nosotros',
+      component: ChatbotComponent,
+      data: { public: true }
+    },
   ];
